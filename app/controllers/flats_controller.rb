de8c4dbd4bf862d614a038bc1175733f33dba094
masoud arabi <arabimasoud88@gmail.com>
2021-01-01 11:24:56 -0500

    add index and show for controller and view

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,18 @@
+require "open-uri"
+
+class FlatsController < ApplicationController
+    
+    
+    def index 
+        url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
+        @flats = JSON.parse(open(url).read)
+        
+    end
+    
+    def show
+        url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
+        @flats = JSON.parse(open(url).read)
+        id = params[:id]
+        @flat = @flats.find { |flat| flat['id'] == params[:id].to_i }
+    end
+end
